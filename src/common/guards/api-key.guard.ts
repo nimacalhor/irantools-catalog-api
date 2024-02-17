a10feb24dc9036508a67e1624851cf6607bdf1d0
nimacalor <nimakalhor25@gmail.com>
2024-02-17 07:31:58 -0800

    add ApiKeyGuard

---
+++ src/common/guards/api-key.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import type { Request } from 'express';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    return this._isValid(request);
+  }
+
+  private _isValid(req: Request) {
+    const { authorization } = req.headers;
+    debugger;
+    return authorization === process.env.API_KEY
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { MongooseExceptionFilter } from './common/filters/mongoose-exception.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new MongooseExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard())
   app.enableCors({ origin: ['http://127.0.0.1:4000'] });
   await app.listen(3000);
 }
